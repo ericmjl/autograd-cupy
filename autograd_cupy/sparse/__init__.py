c5aa6454e7f8ca44482fbc1aa34080f5c10be514
Eric Ma <ericmajinglong@gmail.com>
2018-06-28 14:48:06 -0400

    first commit

---
+++ autograd_cupy/sparse/__init__.py
@@ -0,0 +1,6 @@
+from .sparse_wrapper import *
+from . import sparse_vjps
+from . import sparse_jvps
+from . import sparse_vspaces
+from . import sparse_boxes
+
---
+++ tests/test_cupy_sparse.py
@@ -0,0 +1,44 @@
+import autograd.cupy as cp
+from autograd import elementwise_grad as egrad
+import pytest
+from autograd import grad
+import pdb
+import numpy as np
+from autograd.test_util import check_grads
+
+
+# ----- pytest fixture for sparse arrays ----- #
+@pytest.fixture
+def eye():
+    return cp.sparse.eye(5).tocsr()
+
+
+# ----- tests for array creation ----- #
+@pytest.mark.works
+@pytest.mark.cupy_sparse
+def test_sparse_coo_matrix(sparse):
+    """This just has to not error out."""
+    data = cp.array([1, 2, 3]).astype('float32')
+    rows = cp.array([1, 2, 3]).astype('float32')
+    cols = cp.array([1, 3, 4]).astype('float32')
+    print(sparse.shape)
+
+
+# ----- tests for array multiplication ----- #
+@pytest.mark.works
+@pytest.mark.cupy_sparse
+def test_sparse_dense_multiplication(sparse):
+    """This just has to not error out."""
+    dense = cp.random.random(size=(5, 4))
+    sparse.dot(dense)
+
+
+@pytest.mark.test
+@pytest.mark.cupy_sparse
+def test_sparse_dot(eye):
+    dense = cp.random.random(size=(5, 5))
+
+    def fun(x):
+        return cp.sparse.dot(x, dense)
+
+    check_grads(fun)(eye)
